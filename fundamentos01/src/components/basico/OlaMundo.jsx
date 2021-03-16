67969e45455176b2c0ea26e9aeebab353b35f4d5
migotto7 <elthongamer07@gmail.com>
2021-03-16 14:58:13 +0000

    Aula01 - primeiro component

---
+++ fundamentos01/src/components/basico/OlaMundo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function OlaMundo(){
+    const informacao = 'primeiro componente jsx'
+
+    return(
+        <div>
+            <h2>Componente Ola Mundo</h2>
+            <p>{ informacao }</p>
+        </div>
+    )
+}
+
+export default OlaMundo
